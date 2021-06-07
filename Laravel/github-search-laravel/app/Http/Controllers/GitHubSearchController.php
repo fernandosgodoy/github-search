5a5274047a25d5123e2d45a789c6f2a7edc5389a
Luiz Fernando de Santana <luizfernando.itpro@gmail.com>
2021-06-07 18:24:15 -0300

    retorno da pesquisa

--- Laravel/github-search-laravel/app/Http/Controllers/GitHubSearchController.php
+++ Laravel/github-search-laravel/app/Http/Controllers/GitHubSearchController.php
@@ -4,15 +4,15 @@ namespace App\Http\Controllers;
 
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Support\Facades\Http;
-use Guzzle;
+use GuzzleHttp\Client;
 
 class GitHubSearchController extends BaseController
 {
 
     public function Search($searchQuery) {
-        $client = new GuzzleHttp\Client();
-        $res = $client->request('GET', 'https://searchcode.com/api/' .
-            "codesearch_I/?q=".$searchQuery."&src=2");
+        $client = new Client();
+        $res = $client->request('GET', 'http://searchcode.com/api/' .
+            "codesearch_I/?q=".$searchQuery."&src=2", ['allow_redirects' => false]);
         $res->getStatusCode();
         $res->getHeader('content-type')[0];
         $body = $res->getBody();
@@ -20,6 +20,7 @@ class GitHubSearchController extends BaseController
         // $client = new \GuzzleHttp\Client('https://searchcode.com/api/');
         // $response = $client->get("codesearch_I/?q=".$searchQuery."&src=2")->send();
         return dd($body);
+        // return $body;
     }
 
 }
